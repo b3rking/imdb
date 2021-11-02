747c6754cfde2bfa0778102d6811213397ffa85a
b3rking <berkingdaquan@gmail.com>
2021-11-02 19:29:24 +0200

    created the first view

--- watchmate_app/views.py
+++ watchmate_app/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
+
+def movie_list(request):
+
+    movies = Movie.objects.all()
+
+    return HttpResponse(movies)
