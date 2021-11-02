28d8aa0b31d6cf5026c5d4585809083bd34c5d3b
b3rking <berkingdaquan@gmail.com>
2021-11-02 19:10:12 +0200

    movie model

--- watchmate_app/models.py
+++ watchmate_app/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class Movie(models.Model):
 
-    pass
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+
+        return self.name
